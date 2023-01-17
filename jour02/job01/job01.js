beba0d64cc814aa43becd1fc97b5df48a9793a76
mehdi-romdhani <mehdi.romdhani@laplateforme.io>
2023-01-17 12:24:16 +0100

    Hello DOM

---
+++ jour02/job01/job01.js
@@ -0,0 +1,11 @@
+//Creation script Js
+
+
+const citation = () =>{ // ecriture function fléché ES6
+    let a = document.getElementById('citation');
+    return console.log(a);
+}
+
+
+
+console.log(citation());
