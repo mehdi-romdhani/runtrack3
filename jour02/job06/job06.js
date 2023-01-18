75ac8d6495243c4c37ee839f174a33330865e30f
mehdi-romdhani <mehdi.romdhani@laplateforme.io>
2023-01-18 20:51:12 +0100

    Konami Code for screen something if the code is true

--- jour02/job06/job06.js
+++ jour02/job06/job06.js
@@ -1,6 +1,43 @@
 //Se Declenche lorsque que le document HTML inital à étè completement chargé
 document.addEventListener("DOMContentLoaded", () => {
-        
-    
+  let body = document.querySelector("body"); //On recupere le body pour pouvoir afficher par la suite la couleur
+  let messageWIn = document.querySelector('.container');
+  messageWIn.style.display="none";//au chargement de la page , le message n'apparait pas
+ 
+ 
+  let array_code = [
+    "ArrowUp",
+    "ArrowUp",
+    "ArrowDown",
+    "ArrowDown",
+    "ArrowLeft",
+    "ArrowRight",
+    "ArrowLeft",
+    "ArrowRight",
+    "b",
+    "a",
+  ]; //array qui comporte les commandes à comparer au moment de la saisi du code konami
+
+  let array_vide = []; // array vide dans lequels on vas stocker la valeur d'entrer du code konami pour pouvoir les comparer par la suite
+
+  body.addEventListener("keydown", (event) => {
+    let x = event.key; // stock la saisi du clavier dans une variable pour voir en console si la saisi est bien recuper par l'evenement 
+    //console.log(x);
+
+    let compareArray = array_vide.push(x); // on ajoute les valeurs lors de la saisie clavier dans l'array vide
+    //console.log(compareArray);
+    console.log(array_vide);
+
+    if (array_vide.length !== array_code.length) { // on compare si la longueur des array sont les memes 
+      //console.log("no");
+      return false;//si non le code s'arrete & ne check pas la suite
+    }
+
+    if (array_vide.sort().toString() !== array_code.sort().toString()) {// on verifie avec une condition si le trie ce fais dans l'ordre croissant
+      return false;// si non le code s'arrete 
+    }
+    body.style.backgroundColor = "blue"; // si toute les etapes de verif sont passés, le code est pret à etre exécuté
+    messageWIn.style.display='block';// si victoire , le display repasse à block et le message s'affiche
+
   });
-  
+});
--- jour02/job06/job06.php
+++ jour02/job06/job06.php
@@ -6,13 +6,17 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style.css">
+    <!-- <link rel="stylesheet" href="./style.css"> -->
     <!-- LINK FOLDER JS  -->
     <script src="./job06.js" defer></script>
     
     <title>Job06</title>
 </head>
 <body>
+    <div class="container">
+        <h1>DONE U FIND THE KONAMI CODE</h1>
+        <p>La Plateforme 2022</p>
+    </div>
    
 </body>
 </html>
